91e7a9a3fdc737838fe8680c3b1b6e6ea25fb649
Saad Elbeleidy <sbeleidy90@gmail.com>
2016-10-04 16:25:41 -0600

    Stop tracking toughSample changes and clean up code

--- main.py
+++ main.py
@@ -246,38 +246,15 @@ def plotSolutionSet(points, set=None):
     plt.show()
 
 def genToughPoint():
-    # denseOrNarrow = random.randint(0,1)
 
-    # if denseOrNarrow == 0:
-
-    #     return str(random.randint(500,700))+ " " + str(random.randint(500,700)) + " " + str(random.randint(500,700)) + "\n"
     x = random.randint(0,1)
     y = random.randint(0,1)
     z = random.randint(0,1)
-    # result = {
-    #     '0': random.randint(800,1000),
-    #     '1': random.randint(400,600),
-    #     '2': random.randint(00,600),
-    #     '3': random.randint(500,600),
-    #     '4': random.randint(100,200),
-    #     '5': random.randint(-50,50),
-    #     '6': random.randint(-200,-100),
-    #     '7': random.randint(-600,-500),
-    #     '8': random.randint(-600,-500),
-    #     '9': random.randint(-800,-700),
-    #     '10': random.randint(-1000,-900)
-    # }
+    
     result = {
         '0': lambda x: random.randint(500,1000),
         '1': lambda x: random.randint(-1000,-500)
     }
-
-    # result = {
-    #     '2': lambda x: random.randint(-100,100),
-    #     '3': lambda x: random.randint(-1000,-900),
-    #     '4': lambda x: random.randint(900,1000),
-    #     '5': lambda x: random.randint(500,700)
-    # }
 
     return str(result[str(x)](1))+ " " + str(result[str(y)](1)) + " " + str(result[str(z)](1)) + "\n"
 
@@ -315,8 +292,8 @@ def testing():
     ## Spec Example
     ######
 
-    # Get values from the spec sample
-    n, k, points = getValsFromTxt("SpecSample.txt")
+    # # Get values from the spec sample
+    # n, k, points = getValsFromTxt("SpecSample.txt")
 
     # # Run algorithm for spec sample
     # bestScore, bestSet = useAlgorithm(randomAlg, n, k, points, 100)
@@ -331,16 +308,16 @@ def testing():
     ## Generated Sample
     ######
 
-    # Generate a sample input file then get its values and solve it and output the result in a file
-    genSample("mySample.txt")
-    sampleN, sampleK, samplePoints = getValsFromTxt("mySample.txt")
-    sampleScore, sampleSet = useAlgorithm(randomStartingPointAlgorithm, sampleN, sampleK, samplePoints, sampleN+300)
-    genOutVals("mySampleSolution.txt",sampleScore, sampleSet)
-    plotSolutionSet(samplePoints, sampleSet)
+    # # Generate a sample input file then get its values and solve it and output the result in a file
+    # genSample("mySample.txt")
+    # sampleN, sampleK, samplePoints = getValsFromTxt("mySample.txt")
+    # sampleScore, sampleSet = useAlgorithm(randomStartingPointAlgorithm, sampleN, sampleK, samplePoints, sampleN+300)
+    # genOutVals("mySampleSolution.txt",sampleScore, sampleSet)
+    # plotSolutionSet(samplePoints, sampleSet)
 
-    iterativeScore, iterativeSet = useAlgorithm(iterativeStartingPointsAlgorithm, sampleN, sampleK, samplePoints, 20)
-    genOutVals("mySampleSolution-Iterative.txt",iterativeScore, iterativeSet)
-    plotSolutionSet(samplePoints, iterativeSet)
+    # iterativeScore, iterativeSet = useAlgorithm(iterativeStartingPointsAlgorithm, sampleN, sampleK, samplePoints, 20)
+    # genOutVals("mySampleSolution-Iterative.txt",iterativeScore, iterativeSet)
+    # plotSolutionSet(samplePoints, iterativeSet)
 
     # # Verify solution given k and points
     # if verifySolution(sampleK, samplePoints, "mySampleSolution.txt"):
@@ -356,11 +333,11 @@ def testing():
 
     # plotSolutionSet(samplePoints, sampleSet)
 
-    ## Tough sample
-    toughN, toughK, toughPoints = getValsFromTxt("toughSample.txt")
-    toughScore, toughSet = useAlgorithm(randomStartingPointAlgorithm, toughN, toughK, toughPoints, 100)
-    genOutVals("toughSampleSolution.txt",toughScore, toughSet)
-    plotSolutionSet(toughPoints, toughSet)
+    # ## Tough sample
+    # toughN, toughK, toughPoints = getValsFromTxt("toughSample.txt")
+    # toughScore, toughSet = useAlgorithm(randomStartingPointAlgorithm, toughN, toughK, toughPoints, 100)
+    # genOutVals("toughSampleSolution.txt",toughScore, toughSet)
+    # plotSolutionSet(toughPoints, toughSet)
 
     # genToughPoint()
 
